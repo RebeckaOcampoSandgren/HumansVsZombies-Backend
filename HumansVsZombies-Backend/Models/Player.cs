08ac6c9b8d41f00bf3a3226ec39a2c987c08a8a6
Rebecka Ocampo Sandgren <62889999+RebeckaOcampoSandgren@users.noreply.github.com>
2022-08-31 14:15:47 +0200

    feat: Added properties to models
    Properties and relationships added to models: Player, Squad, SquadCheckin, SquadMember and User

--- HumansVsZombies-Backend/Models/Player.cs
+++ HumansVsZombies-Backend/Models/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,24 @@ namespace HumansVsZombies_Backend.Models
 {
     public class Player
     {
+        //PK
+        [Required]
+        public int PlayerId { get; set; }
+        [Required]
+        public bool IsHuman { get; set; }
+        [Required]
+        public bool IsPatientZero { get; set; }
+        [Required]
+        public int BiteCode { get; set; }
+
+        //Not sure about the relationship
+        [Required]
+        public int UserId { get; set; }
+        public User User { get; set; }
+        [Required]
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+        public virtual ICollection<Kill> Kills { get; set; } 
+
     }
 }
