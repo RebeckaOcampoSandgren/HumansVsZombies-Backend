2ca63e4621e7eff1ead4ad1a2f4c2c3777cad85f
betielWork <betiel.yohannes.work@gmail.com>
2022-09-02 16:22:20 +0200

    feat: added profile to models
    added profiles files to models: squad, squadmember, player

--- HumansVsZombies-Backend/Profiles/SquadProfile.cs
+++ HumansVsZombies-Backend/Profiles/SquadProfile.cs
@@ -1,11 +1,38 @@
-﻿using System;
+﻿using AutoMapper;
+using HumansVsZombies_Backend.DTOs.SquadDTO;
+using HumansVsZombies_Backend.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace HumansVsZombies_Backend.Profiles
 {
-    public class SquadProfile
+    public class SquadProfile : Profile
     {
+        public SquadProfile()
+        {
+            //Squad<->SquadReadDTO
+            CreateMap<Squad, SquadReadDTO>()
+            .ForMember(g => g.Game, opt => opt
+               .MapFrom(g => g.GameId))
+               .ForMember(c => c.Chats, opt => opt
+               .MapFrom(c => c.Chats.Select(x => x.ChatId).ToArray()))
+               .ForMember(s => s.SquadCheckins, opt => opt
+               .MapFrom(s => s.SquadCheckins.Select(x => x.SquadCheckinId).ToArray()))
+               .ForMember(s => s.SquadMembers, opt => opt
+               .MapFrom(s => s.SquadMembers.Select(x => x.SquadMemberId).ToArray()))
+               .ReverseMap();
+
+            //Squad<->SquadCreateDTO
+            CreateMap<Squad, SquadCreateDTO>().ReverseMap();
+
+
+            //Squad<->SquadUpdateDTO
+            CreateMap<Squad, SquadUpdateDTO>()
+                .ForMember(g => g.GameId, opt => opt
+                .MapFrom(g => g.GameId))
+                .ReverseMap();
+        }
     }
 }
